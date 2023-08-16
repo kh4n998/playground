a83ed5f571d6e075562e2ff92f07befb9f3316b2
Bùi Đình Khang <chocopie.nuce@gmail.com>
2023-08-16 13:55:50 +0700

    commit source code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'playground',
+    loadChildren: () => import("./playground/playground.module").then(m => m.PlaygroundModule)
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'playground'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
